39be8545bc3050acf69d97099a3f2e0c52b6a23a
Markus Winter <m.winter@sap.com>
2026-05-06 14:12:18 +0100

    Update release-pipeline.groovy

--- vars/release-pipeline.groovy
+++ vars/release-pipeline.groovy
@@ -1,3 +1,4 @@
 parameters {
    string(name: 'ENVIRONMENT', defaultValue: 'staging', description: 'Target deployment environment')
+   string(name: 'ROLES', defaultValue: 'admin', description: 'Roles')
 }
